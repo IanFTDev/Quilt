df6a64a3a9be18dfeb861a5683ef533941cba26e
root <root@ToucansNest>
2025-10-01 11:48:36 -0400

    Added login restrictions

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, logout_user, login_required, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -17,16 +18,20 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in succesfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash("Incorrect password, try again.", category='error')
         else:
             flash("Incorrect email, try again.", category='error')
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -52,6 +57,8 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash('Account Created!', category='success')
+            login_user(new_user, remember=True)
+
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
